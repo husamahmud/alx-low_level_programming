d48f9fcf1dc2e10f69ad13ef6b16e50f7e9e9378
Hüsam <husamahmud@gmail.com>
2023-06-15 14:58:11 +0300

    C program that prints s string with puts func.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a string using puts func.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
