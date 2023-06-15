b3f94f011e8ac53cbe5bd7a1a88f190ad8b3df2d
Hüsam <husamahmud@gmail.com>
2023-06-15 19:46:18 +0300

    C program that prints a string with printf func.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints a string using printf func.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
