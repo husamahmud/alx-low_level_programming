632d1ed08292f194c36b38857ed295b6da38bea0
Hüsam <husamahmud@gmail.com>
2023-06-16 16:17:12 +0300

    C program that prints all digits from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints all digits from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 10; i++)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
