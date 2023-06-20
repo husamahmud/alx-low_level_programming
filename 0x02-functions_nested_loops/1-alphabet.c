067513e48386281a38ba6de01fc1498799e08534
Hüsam <husamahmud@gmail.com>
2023-06-20 16:16:02 +0300

    function that prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
-
-/**
- * print_alphabet - prints the alphabet in lowercase
- */
-
-void print_alphabet(void)
-{
-int i;
-for (i = 'a'; i <= 'z'; i++)
-{
-putchar(i);
-}
-putchar('\n');
-}
+#include "main.h"
 
 /**
  * main - Entry point
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,13 @@ void print_putchar(void)
 {
   printf("_putchar\n");
 }
+
+void print_alphabet(void)
+{
+  int i;
+  for (i = 'a'; i <= 'z'; i++)
+  {
+    putchar(i);
+  }
+  putchar('\n');
+}
