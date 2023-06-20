0a1f43aaa606cb8bc9fd75a3497d1c07db05facd
Hüsam <husamahmud@gmail.com>
2023-06-21 00:48:00 +0300

    function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,10 +2,6 @@
 
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer
- *
- * Description: program that prints every minute of the day of Jack Bauer
- *
- * Return: Always 0 (Success)
  */
 
 void jack_bauer(void)
