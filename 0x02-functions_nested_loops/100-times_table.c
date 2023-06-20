c470848aa46b34927ebc156da94e72dc736570b5
Hüsam <husamahmud@gmail.com>
2023-06-21 02:39:43 +0300

    function that prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,6 +2,7 @@
 
 /**
  * print_times_table - function that prints the n times table
+ * @n: prints the n times table
  */
 
 void print_times_table(int n)
@@ -9,16 +10,19 @@ void print_times_table(int n)
 	int i, j;
 
 	if (n > 15 || n < 0)
-		return;
+		return (0);
 
-	printf("0");
-
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= n; i++)
 	{
 		printf("0");
-		for (j = 1; j <= 9; j++)
+		for (j = 1; j <= n; j++)
 		{
-			(i * j < 10) ? printf(",   %d", i * j) : printf(",  %d", i * j);
+			if (i * j < 10)
+				printf(",   %d", i * j);
+			else if (i * j < 100)
+				printf(",  %d", i * j);
+			else
+				printf(", %d", i * j);
 		}
 		printf("\n");
 	}
