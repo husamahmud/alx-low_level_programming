d8abacba775129d4d281b6d971c1ae41d086dffb
Hüsam <husamahmud@gmail.com>
2023-06-20 16:39:45 +0300

    program that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "1-main.c"
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,8 @@
+void print_alphabet(void)
+{
+  int i;
+  for (i = 'a'; i < 'z'; i++)
+    _putchar(i);
+
+  _putchar('\n');
+}
