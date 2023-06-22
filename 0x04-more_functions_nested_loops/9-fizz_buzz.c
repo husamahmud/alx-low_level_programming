d53a10531d73f528b8e8ef116ab783f71b2dc104
Hüsam <husamahmud@gmail.com>
2023-06-22 20:00:53 +0300

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,9 +19,9 @@ int main(void)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
 			printf("FizzBuzz ");
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
