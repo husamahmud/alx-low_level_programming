9c8a30c87aa58feafd4444684dd0849fea8d163e
Hüsam <husamahmud@gmail.com>
2023-06-26 21:37:33 +0300

    prints a character and skip the next one in the string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,12 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str += 2;
-	}
+	int len = 0, i;
+
+	while (str[len] != '\0')
+		len++;
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
