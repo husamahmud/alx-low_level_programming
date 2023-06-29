055db53081b214a335f3b824efeebc3de9ca0f57
Hüsam <husamahmud@gmail.com>
2023-06-29 23:00:30 +0300

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,9 +15,11 @@ char *cap_string(char *str)
 		if (str[0] >= 'a' && str[0] <= 'z')
 			str[0] -= 32;
 
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.' || str[i] == ',' ||
-				str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-				str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == '.' || str[i] == ',' || str[i] == ';' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 
