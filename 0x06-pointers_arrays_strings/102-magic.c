42036a7710fd030ab355180c958bb02fc47a5f91
Hüsam <husamahmud@gmail.com>
2023-06-30 02:33:02 +0300

    modify a memory location

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- *
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	int n;
@@ -16,16 +9,15 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-		* write your line of code here...
-		* Remember:
-		* - you are not allowed to use a
-		* - you are not allowed to modify p
-		* - only one statement
-		* - you are not allowed to code anything else than this line of code
-		*/
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
