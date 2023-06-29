5bf1f8fe58329bc66ebe220a46811ad3fce3c2ff
Hüsam <husamahmud@gmail.com>
2023-06-30 02:11:07 +0300

    function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int digit;
+	digit = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		digit = -n;
+	}
+
+	if (digit / 10 != 0)
+	{
+		print_number(digit / 10);
+	}
+	_putchar((digit % 10) + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
