94724d10b2f40fa50c366eb3fcea8b9810b4ab93
iamhusam <iamhusamg@gmail.com>
2023-07-08 10:48:48 -0700

    my first static lib

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: this is a disc
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+"\t- Kevin Mitnick");
+
+return (0);
+}
