622ecc12acc7e3613dd84c11a0f81ebc99e0c879
Hüsam <husamahmud@gmail.com>
2023-07-08 06:01:18 +0300

    shga

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be
-the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+	      "\t- Kevin Mitnick");
+	return 0;
 }
