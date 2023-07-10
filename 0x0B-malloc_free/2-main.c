bfd795db277378460a47afaad55524d60665e66b
Hüsam <husamahmud@gmail.com>
2023-07-10 19:00:59 +0300

    2

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -11,7 +11,25 @@ int main(void)
 {
 	char *s;
 
-	s = str_concat("Betty ", "Holberton");
+	s = str_concat("Hello", NULL);
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+
+	s = str_concat(NULL, "Hello");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+
+	s = str_concat(NULL, NULL);
 	if (s == NULL)
 	{
 		printf("failed\n");
