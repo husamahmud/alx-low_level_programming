272ebfd9c73af191974ab46a0965343d87d08aa0
Hüsam <husamahmud@gmail.com>
2023-07-13 13:46:36 +0300

    Modify task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,11 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *str;
+	void *ptr;
 
-	str = malloc(b);
-	if (str == NULL)
+	ptr = malloc(b);
+	if (ptr == NULL)
 		exit(98);
 
-	return (str);
+	return (ptr);
 }
+
