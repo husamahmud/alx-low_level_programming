a558032bd8d0dca458ab26242734d5266bafbb4e
Hüsam <husamahmud@gmail.com>
2023-07-17 18:47:03 +0300

    Edit task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, name);
+	if (d == NULL)
+		return;
+	d->name = name;
 	d->age = age;
-	strcpy(d->owner, owner);
+	d->owner = owner;
 }
