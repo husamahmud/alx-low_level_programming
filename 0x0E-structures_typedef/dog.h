c6bb783dc8477814153f3396b0aa840b0b62ba54
Hüsam <husamahmud@gmail.com>
2023-07-17 18:39:37 +0300

    Edit headr file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,23 +14,16 @@
  * This structure represents information about a dog,
  * such as its name, age, and owner.
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-typedef struct
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
