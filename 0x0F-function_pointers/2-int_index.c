1676ae56ad66e03a0ff55d7b83d9f0fc53f19d1e
Hüsam <husamahmud@gmail.com>
2023-07-19 17:59:51 +0300

    Edit task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
 
-	return (0);
+	return (-1);
 }
