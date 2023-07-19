b07e488edfd775ce8d498c449dbdc66b508359cd
Hüsam <husamahmud@gmail.com>
2023-07-19 17:57:29 +0300

    Add header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef ALX_LOW_LEVEL_PROGRAMMING_FUNCTION_POINTERS_H
+#define ALX_LOW_LEVEL_PROGRAMMING_FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
