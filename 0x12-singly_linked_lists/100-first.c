1e86b82bd0de6ee07134ed82d182c2594bd1fd02
Hüsam <husamahmud@gmail.com>
2023-07-27 16:24:32 +0300

    Add task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void foo(void) __attribute__ ((constructor));
 
 /**
@@ -5,6 +7,6 @@ void foo(void) __attribute__ ((constructor));
  */
 void foo(void)
 {
-	printf("You're beat! and yet, you must allow,");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("nI bore my house upon my back!\n");
 }
