c54f788e1c03c0179ed6fe91dc79ea008c7e0b48
Hüsam <husamahmud@gmail.com>
2023-07-27 16:23:17 +0300

    Add task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+void foo(void) __attribute__ ((constructor));
+
 /**
  * foo - function that prints a string before main func.
  */
-__attribute__((constructor))
 void foo(void)
 {
 	printf("You're beat! and yet, you must allow,");
