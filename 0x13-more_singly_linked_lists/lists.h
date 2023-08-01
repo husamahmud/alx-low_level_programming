8c9d4ea0a1e0d70b2d3f76467336de2fa82e4c50
Hüsam <husamahmud@gmail.com>
2023-08-01 20:35:00 +0300

    Update header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 /**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
+/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
