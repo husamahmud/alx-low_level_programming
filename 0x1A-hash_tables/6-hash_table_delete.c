a7f68c57febc52a8a0075d0ceb520c0d89b549d1
iamhusam <husamahmud@gmail.com>
2023-11-03 16:47:32 +0300

    Add 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	hash_node_t *temp;
+
+	/* check for invalid input param */
+	if (ht == NULL)
+		return;
+
+	/* interate through each index in the hash table */
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		/* frees the nodes in the linked list */
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	/* free the array and the hash table struct */
+	free(ht->array);
+	free(ht);
+}
