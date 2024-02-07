47adcd98d9e75b137780c07ea2ed122d47ff4fbf
Hüsam <husammahmud@Husams-MacBook-Air.local>
2024-02-07 18:39:11 +0300

    Add 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,6 +12,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (!array)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
