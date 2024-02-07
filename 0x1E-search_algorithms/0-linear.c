ff239fb3e6962b3455e000a63ee4a52ee1e9b863
Hüsam <husammahmud@Husams-MacBook-Air.local>
2024-02-07 18:52:05 +0300

    Add 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
   * linear_search - search for an element in an array
   * @array: the array we want to search in
@@ -10,13 +11,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
